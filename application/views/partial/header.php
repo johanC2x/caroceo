caa4a9cbaa7d27b4ea295c0d8d62bf64b75c265c
johanC <johins2x@gmail.com>
2016-11-07 21:05:39 -0500

    primer oceo

---
+++ application/views/partial/header.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+	"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head> 
+<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+<base href="<?php echo base_url();?>" />
+<title>OCEO</title>
+<link rel="stylesheet" rev="stylesheet" href="<?php echo base_url();?>css/bootstrap.min.css"/>
+<link rel="stylesheet" rev="stylesheet" href="<?php echo base_url();?>css/animate.css" />
+<link rel="stylesheet" rev="stylesheet" href="<?php echo base_url();?>css/eshop.css" />
+<link rel="stylesheet" rev="stylesheet" href="<?php echo base_url();?>css/font-awesome.min.css" />
+<link rel="stylesheet" rev="stylesheet" href="<?php echo base_url();?>css/main.css" />
+<link rel="stylesheet" rev="stylesheet" href="<?php echo base_url();?>css/prettyPhoto.css" />
+<link rel="stylesheet" rev="stylesheet" href="<?php echo base_url();?>css/price-range.css" />
+<link rel="stylesheet" rev="stylesheet" href="<?php echo base_url();?>css/responsive.css" />
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
+
+<style type="text/css">
+	html {
+		overflow: auto;
+	}
+</style>
+</head>
+<body>
