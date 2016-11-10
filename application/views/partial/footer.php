5af47ff717c040ba22971b4d910ac26bedd5da8f
Sergio Renato Mendoza Pisconte <renato.mpisconte@gmail.com>
2016-11-10 11:30:53 -0500

    Subiendo estructura dle proyecto

--- application/views/partial/footer.php
+++ application/views/partial/footer.php
@@ -1,11 +1,11 @@
-<script src="<?php echo base_url();?>js/jquery.js" type="text/javascript" language="javascript" charset="UTF-8"></script>
-<script src="<?php echo base_url();?>js/bootstrap.min.js" type="text/javascript" language="javascript" charset="UTF-8"></script>
-<script src="<?php echo base_url();?>js/html5shiv.js" type="text/javascript" language="javascript" charset="UTF-8"></script>
-<script src="<?php echo base_url();?>js/jquery.prettyPhoto.js" type="text/javascript" language="javascript" charset="UTF-8"></script>
-<script src="<?php echo base_url();?>js/jquery.scrollUp.min.js" type="text/javascript" language="javascript" charset="UTF-8"></script>
-<script src="<?php echo base_url();?>js/price-range.js" type="text/javascript" language="javascript" charset="UTF-8"></script>
-<script src="<?php echo base_url();?>js/funciones/inicio.js" type="text/javascript" language="javascript" charset="UTF-8"></script>
-<script src="<?php echo base_url();?>js/funciones/funciones.js" type="text/javascript" language="javascript" charset="UTF-8"></script>
+<script src="<?php echo base_url();?>assets/js/jquery.js" type="text/javascript" language="javascript" charset="UTF-8"></script>
+<script src="<?php echo base_url();?>assets/js/bootstrap.min.js" type="text/javascript" language="javascript" charset="UTF-8"></script>
+<script src="<?php echo base_url();?>assets/js/html5shiv.js" type="text/javascript" language="javascript" charset="UTF-8"></script>
+<script src="<?php echo base_url();?>assets/js/jquery.prettyPhoto.js" type="text/javascript" language="javascript" charset="UTF-8"></script>
+<script src="<?php echo base_url();?>assets/js/jquery.scrollUp.min.js" type="text/javascript" language="javascript" charset="UTF-8"></script>
+<script src="<?php echo base_url();?>assets/js/price-range.js" type="text/javascript" language="javascript" charset="UTF-8"></script>
+<script src="<?php echo base_url();?>assets/js/funciones/inicio.js" type="text/javascript" language="javascript" charset="UTF-8"></script>
+<script src="<?php echo base_url();?>assets/js/funciones/funciones.js" type="text/javascript" language="javascript" charset="UTF-8"></script>
 <script type="text/javascript">
     $("#lk1").hover(function(){
         $("#d1").css("display", "block");
--- application/views/partial/header.php
+++ application/views/partial/header.php
@@ -5,14 +5,14 @@
 <meta http-equiv="content-type" content="text/html; charset=utf-8" />
 <base href="<?php echo base_url();?>" />
 <title>OCEO</title>
-<link rel="stylesheet" rev="stylesheet" href="<?php echo base_url();?>css/bootstrap.min.css"/>
-<link rel="stylesheet" rev="stylesheet" href="<?php echo base_url();?>css/animate.css" />
-<link rel="stylesheet" rev="stylesheet" href="<?php echo base_url();?>css/eshop.css" />
-<link rel="stylesheet" rev="stylesheet" href="<?php echo base_url();?>css/font-awesome.min.css" />
-<link rel="stylesheet" rev="stylesheet" href="<?php echo base_url();?>css/main.css" />
-<link rel="stylesheet" rev="stylesheet" href="<?php echo base_url();?>css/prettyPhoto.css" />
-<link rel="stylesheet" rev="stylesheet" href="<?php echo base_url();?>css/price-range.css" />
-<link rel="stylesheet" rev="stylesheet" href="<?php echo base_url();?>css/responsive.css" />
+<link rel="stylesheet" rev="stylesheet" href="<?php echo base_url();?>assets/css/bootstrap.min.css"/>
+<link rel="stylesheet" rev="stylesheet" href="<?php echo base_url();?>assets/css/animate.css" />
+<link rel="stylesheet" rev="stylesheet" href="<?php echo base_url();?>assets/css/eshop.css" />
+<link rel="stylesheet" rev="stylesheet" href="<?php echo base_url();?>assets/css/font-awesome.min.css" />
+<link rel="stylesheet" rev="stylesheet" href="<?php echo base_url();?>assets/css/main.css" />
+<link rel="stylesheet" rev="stylesheet" href="<?php echo base_url();?>assets/css/prettyPhoto.css" />
+<link rel="stylesheet" rev="stylesheet" href="<?php echo base_url();?>assets/css/price-range.css" />
+<link rel="stylesheet" rev="stylesheet" href="<?php echo base_url();?>assets/css/responsive.css" />
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
 
 <style type="text/css">
